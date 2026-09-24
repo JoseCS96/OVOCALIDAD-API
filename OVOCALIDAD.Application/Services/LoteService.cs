6f351c550a0ea63ba26b2f0d6feed92b47a54feb
JoseCS96 <140642015+JoseCS96@users.noreply.github.com>
2026-09-24 17:12:30 -0500

    Implement lot catalogs service

--- OVOCALIDAD.Application/Services/LoteService.cs
+++ OVOCALIDAD.Application/Services/LoteService.cs
@@ -7,20 +7,14 @@ public class LoteService : ILoteService
 {
     private readonly ILoteRepository _loteRepository;
 
-    public LoteService(ILoteRepository loteRepository)
-    {
-        _loteRepository = loteRepository;
-    }
+    public LoteService(ILoteRepository loteRepository) => _loteRepository = loteRepository;
 
-    public async Task<GenerarLoteResponse> GenerarLoteAsync(
-        GenerarLoteRequest request)
-    {
-        return await _loteRepository.GenerarLoteAsync(request);
-    }
+    public Task<GenerarLoteResponse> GenerarLoteAsync(GenerarLoteRequest request) =>
+        _loteRepository.GenerarLoteAsync(request);
 
-    public async Task<IReadOnlyList<LoteListadoDto>> ListarLotesAsync(
-        ListarLotesFiltro filtro)
-    {
-        return await _loteRepository.ListarLotesAsync(filtro);
-    }
+    public Task<IReadOnlyList<LoteListadoDto>> ListarLotesAsync(ListarLotesFiltro filtro) =>
+        _loteRepository.ListarLotesAsync(filtro);
+
+    public Task<CatalogosLoteDto> ObtenerCatalogosAsync() =>
+        _loteRepository.ObtenerCatalogosAsync();
 }
