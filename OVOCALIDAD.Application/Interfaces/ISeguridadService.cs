cf674d5bd989d3f6a65479cba7863292d800b721
JoseCS96 <140642015+JoseCS96@users.noreply.github.com>
2026-09-25 17:58:23 -0500

    feat(notifications): extend security service contract

--- OVOCALIDAD.Application/Interfaces/ISeguridadService.cs
+++ OVOCALIDAD.Application/Interfaces/ISeguridadService.cs
@@ -5,4 +5,7 @@ namespace OVOCALIDAD.Application.Interfaces;
 public interface ISeguridadService
 {
     Task<AccesosUsuarioDto?> ObtenerAccesosUsuarioAsync(string nombreUsuario);
+    Task<IReadOnlyList<NotificacionDto>> ObtenerNotificacionesAsync(string nombreUsuario);
+    Task<OperacionNotificacionDto> MarcarNotificacionLeidaAsync(long notificacionId, string nombreUsuario);
+    Task<OperacionNotificacionDto> MarcarNotificacionesModalMostradasAsync(string nombreUsuario);
 }
