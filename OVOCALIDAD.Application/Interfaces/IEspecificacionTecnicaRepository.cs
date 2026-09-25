425e06beda43c6b9181e0e58f890d5354ea5c057
JoseCS96 <140642015+JoseCS96@users.noreply.github.com>
2026-09-25 00:34:13 -0500

    feat(et): add listing contracts

--- OVOCALIDAD.Application/Interfaces/IEspecificacionTecnicaRepository.cs
+++ OVOCALIDAD.Application/Interfaces/IEspecificacionTecnicaRepository.cs
@@ -9,4 +9,5 @@ public interface IEspecificacionTecnicaRepository
     Task<GuardarInformacionGeneralEtResponse> GuardarInformacionGeneralAsync(int versionId, GuardarInformacionGeneralEtRequest request);
     Task<GuardarCaracteristicaEtResponse> GuardarCaracteristicaAsync(int versionId, GuardarCaracteristicaEtRequest request);
     Task<EliminarCaracteristicaEtResponse> EliminarCaracteristicaAsync(int versionId, int versCaractId, EliminarCaracteristicaEtRequest request);
+    Task<IReadOnlyList<EspecificacionTecnicaListadoDto>> ListarAsync(ListarEspecificacionesTecnicasFiltro filtro);
 }
