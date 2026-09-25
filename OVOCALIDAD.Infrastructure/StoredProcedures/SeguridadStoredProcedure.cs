2da48ea4881d238fecacde71b51bee170afd9bd1
JoseCS96 <140642015+JoseCS96@users.noreply.github.com>
2026-09-25 16:15:46 -0500

    feat(seguridad): SeguridadStoredProcedure.cs

---
+++ OVOCALIDAD.Infrastructure/StoredProcedures/SeguridadStoredProcedure.cs
@@ -0,0 +1,63 @@
+using Microsoft.Data.SqlClient;
+using OVOCALIDAD.Application.DTOs.Seguridad;
+using OVOCALIDAD.Infrastructure.Mappers;
+using SPNames = OVOCALIDAD.Shared.Constants.StoredProcedures;
+
+namespace OVOCALIDAD.Infrastructure.StoredProcedures;
+
+public class SeguridadStoredProcedure
+{
+    private readonly StoredProcedureExecutor _executor;
+
+    public SeguridadStoredProcedure(StoredProcedureExecutor executor)
+    {
+        _executor = executor;
+    }
+
+    public async Task<AccesosUsuarioDto?> ObtenerAccesosUsuarioAsync(string nombreUsuario)
+    {
+        var parametros = new List<SqlParameter>
+        {
+            new("@NombreUsuario", nombreUsuario)
+        };
+
+        using var reader = await _executor.ExecuteReaderAsync(
+            SPNames.SP_OBTENER_ACCESOS_USUARIO,
+            parametros);
+
+        if (!await reader.ReadAsync())
+            return null;
+
+        var codigoResultado = reader.GetInt32(reader.GetOrdinal("CodigoResultado"));
+        if (codigoResultado != 0)
+            return null;
+
+        var usuario = reader.MapTo<UsuarioAccesoDto>();
+
+        var perfiles = new List<PerfilAccesoDto>();
+        if (await reader.NextResultAsync())
+            while (await reader.ReadAsync())
+                perfiles.Add(reader.MapTo<PerfilAccesoDto>());
+
+        var modulos = new List<ModuloAccesoDto>();
+        if (await reader.NextResultAsync())
+            while (await reader.ReadAsync())
+                modulos.Add(reader.MapTo<ModuloAccesoDto>());
+
+        var permisos = new List<string>();
+        if (await reader.NextResultAsync())
+        {
+            var ordinalPermisoCodigo = reader.GetOrdinal("PermisoCodigo");
+            while (await reader.ReadAsync())
+                permisos.Add(reader.GetString(ordinalPermisoCodigo));
+        }
+
+        return new AccesosUsuarioDto
+        {
+            Usuario = usuario,
+            Perfiles = perfiles,
+            Modulos = modulos,
+            Permisos = permisos
+        };
+    }
+}
