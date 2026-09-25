5bcfc6f213ef10a5e46db857d0a2a0b23c6ff622
JoseCS96 <140642015+JoseCS96@users.noreply.github.com>
2026-09-24 23:44:58 -0500

    feat(et): add EspecificacionTecnicaRepository.cs

---
+++ OVOCALIDAD.Infrastructure/Repositories/EspecificacionTecnicaRepository.cs
@@ -0,0 +1,18 @@
+using OVOCALIDAD.Application.DTOs.EspecificacionesTecnicas;
+using OVOCALIDAD.Application.Interfaces;
+using OVOCALIDAD.Infrastructure.StoredProcedures;
+
+namespace OVOCALIDAD.Infrastructure.Repositories;
+
+public class EspecificacionTecnicaRepository : IEspecificacionTecnicaRepository
+{
+    private readonly EspecificacionTecnicaStoredProcedure _storedProcedure;
+
+    public EspecificacionTecnicaRepository(EspecificacionTecnicaStoredProcedure storedProcedure)
+    {
+        _storedProcedure = storedProcedure;
+    }
+
+    public Task<CatalogosEspecificacionTecnicaDto> ObtenerCatalogosAsync() =>
+        _storedProcedure.ObtenerCatalogosAsync();
+}
