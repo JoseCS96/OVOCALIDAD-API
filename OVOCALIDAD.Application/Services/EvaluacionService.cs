48b079a530ec4a1b67b05a3b52f01347b8b74467
JoseCS96 <140642015+JoseCS96@users.noreply.github.com>
2026-09-24 20:07:19 -0500

    feat: add online evaluation API

---
+++ OVOCALIDAD.Application/Services/EvaluacionService.cs
@@ -0,0 +1,20 @@
+using OVOCALIDAD.Application.DTOs.Evaluaciones;
+using OVOCALIDAD.Application.Interfaces;
+
+namespace OVOCALIDAD.Application.Services;
+
+public class EvaluacionService : IEvaluacionService
+{
+    private readonly IEvaluacionRepository _repository;
+
+    public EvaluacionService(IEvaluacionRepository repository) => _repository = repository;
+
+    public Task<IniciarEvaluacionResponse> IniciarAsync(int evaluacionId, IniciarEvaluacionRequest request) =>
+        _repository.IniciarAsync(evaluacionId, request);
+
+    public Task<EvaluacionDto?> ObtenerAsync(int evaluacionId) =>
+        _repository.ObtenerAsync(evaluacionId);
+
+    public Task<GuardarResultadoResponse> GuardarResultadoAsync(int evaluacionId, GuardarResultadoRequest request) =>
+        _repository.GuardarResultadoAsync(evaluacionId, request);
+}
