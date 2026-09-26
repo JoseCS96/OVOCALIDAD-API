0b274665d6a1cafbc3181c2665c07fe8863e32a9
JoseCS96 <140642015+JoseCS96@users.noreply.github.com>
2026-09-25 22:07:26 -0500

    refactor: propagate authenticated evaluation user

--- OVOCALIDAD.Application/Interfaces/IEvaluacionRepository.cs
+++ OVOCALIDAD.Application/Interfaces/IEvaluacionRepository.cs
@@ -7,6 +7,6 @@ public interface IEvaluacionRepository
     Task<IniciarEvaluacionResponse> IniciarAsync(int evaluacionId, string usuario);
     Task<PanelEvaluadorDto> ObtenerPanelAsync(string usuario);
     Task<EvaluacionDto?> ObtenerAsync(int evaluacionId);
-    Task<GuardarResultadoResponse> GuardarResultadoAsync(int evaluacionId, GuardarResultadoRequest request);
-    Task<CerrarEvaluacionResponse> CerrarAsync(int evaluacionId, CerrarEvaluacionRequest request);
+    Task<GuardarResultadoResponse> GuardarResultadoAsync(int evaluacionId, GuardarResultadoRequest request, string usuario);
+    Task<CerrarEvaluacionResponse> CerrarAsync(int evaluacionId, string usuario);
 }
