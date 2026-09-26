fa1208490f8f289357dea7163b3dbbe0ccb3b172
JoseCS96 <140642015+JoseCS96@users.noreply.github.com>
2026-09-25 21:44:27 -0500

    feat: register evaluator panel stored procedure

--- OVOCALIDAD.Shared/Constants/StoredProcedures.cs
+++ OVOCALIDAD.Shared/Constants/StoredProcedures.cs
@@ -27,6 +27,7 @@ public static class StoredProcedures
 
     public const string SP_CREAR_EVALUACION = "SP_CREAR_EVALUACION";
     public const string SP_INICIAR_EVALUACION = "SP_INICIAR_EVALUACION";
+    public const string SP_OBTENER_PANEL_EVALUADOR = "SP_OBTENER_PANEL_EVALUADOR";
     public const string SP_OBTENER_EVALUACION = "SP_OBTENER_EVALUACION";
     public const string SP_GUARDAR_RESULTADO = "SP_GUARDAR_RESULTADO";
     public const string SP_CERRAR_EVALUACION = "SP_CERRAR_EVALUACION";
